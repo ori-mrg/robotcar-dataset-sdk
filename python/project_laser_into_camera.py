bc05a785ddc21713ccdc17229ebd285b6f41a7a4
Matt Gadd <mattgadd@robots.ox.ac.uk>
2019-04-15 13:53:27 +0100

    python sdk can project lasers into images using RTK solutions

--- python/project_laser_into_camera.py
+++ python/project_laser_into_camera.py
@@ -42,7 +42,7 @@ with open(extrinsics_path) as extrinsics_file:
 G_camera_vehicle = build_se3_transform(extrinsics)
 G_camera_posesource = None
 
-poses_type = re.search('(vo|ins)\.csv', args.poses_file).group(1)
+poses_type = re.search('(vo|ins|rtk)\.csv', args.poses_file).group(1)
 if poses_type == 'ins':
     with open(os.path.join(args.extrinsics_dir, 'ins.txt')) as extrinsics_file:
         extrinsics = next(extrinsics_file)
